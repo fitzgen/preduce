800ad832e00a05dfd5e5a1db8fdf1f9e27357682
Nick Fitzgerald <fitzgen@gmail.com>
2017-05-05 20:28:04 -0700

    Port all of the clang-delta passes

---
+++ reducers/clang-delta-remove-enum-member-value.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+from reducer_utils import clang_delta_reducer
+
+def main():
+    clang_delta_reducer(sys.argv[1], "remove-enum-member-value")
+
+if __name__ == "__main__":
+    main()
---
+++ tests/fixtures/wow.cpp
@@ -0,0 +1,19 @@
+template <int N>
+struct Wow {
+    char such[N];
+
+    template <class T>
+    struct Such {
+        T t;
+
+        struct Many {
+            bool b;
+            int i;
+            char c;
+        };
+    };
+};
+
+long long beard() {
+    return 42;
+}
